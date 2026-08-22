cf36dce348d2a4c9bfe89b67492788e6038632df
jadaunrinky98@gmail.com <jadaunrinky98@gmail.com>
2026-08-22 13:03:02 +0530

    Add order validation and order book sorting

--- engine/order_book.py
+++ engine/order_book.py
@@ -6,9 +6,24 @@ class OrderBook:
     def add_order(self, order):
         if order["side"] == "BUY":
             self.buy_orders.append(order)
+            self.buy_orders.sort(
+                key=lambda x: x["price"],
+                reverse=True
+            )
+
         elif order["side"] == "SELL":
             self.sell_orders.append(order)
+            self.sell_orders.sort(
+                key=lambda x: x["price"]
+            )
 
     def show_orders(self):
-        print("BUY Orders:", self.buy_orders)
-        print("SELL Orders:", self.sell_orders)
+        print("\n=== ORDER BOOK ===")
+
+        print("BUY Orders:")
+        for order in self.buy_orders:
+            print(order)
+
+        print("\nSELL Orders:")
+        for order in self.sell_orders:
+            print(order)
