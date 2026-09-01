bbb3fde01748110d6aded74327c868326b4c3728
jadaunrinky98@gmail.com <jadaunrinky98@gmail.com>
2026-09-01 20:42:25 +0530

    Update Cython build setup

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
     ext_modules=cythonize(
         [
             "engine/matching_engine.pyx",
-            "engine/order_book.pyx"
+            "engine/order_book.pyx",
+            "engine/ring_buffer.pyx",
+            "engine/c_order.pyx",
+            "engine/c_matcher.pyx"
         ],
         compiler_directives={
             "language_level": "3"
